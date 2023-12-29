aae44b94e0bd5872e60ad85732dc2becba13291a
Ameerhamza313 <ameerhamzakhalid313@gmail.com>
2023-12-29 15:09:13 +0530

    updated

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>,
-	document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+    </BrowserRouter>
+  </>
 );
